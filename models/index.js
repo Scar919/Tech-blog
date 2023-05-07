1dc22d1f80876e48ed96c50ec5b1723320ac6191
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-05-07 16:25:59 -0700

    added to js folders

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const Post = require('./Post');
+const User = require('/Users');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'    
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+     foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
